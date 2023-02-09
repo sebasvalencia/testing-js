3b76508aaf12d3e9b67f0e00a8fa0e16efefefd5
sebasvalencia <sebastian.valencian@gmail.com>
2023-02-09 18:50:07 -0500

    feat: more unit test

---
+++ demos/src/02-math.js
@@ -0,0 +1,43 @@
+function sum(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (b === 0) return null;
+  return a / b;
+}
+
+function secondsConverter(seconds) {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const residue = Math.floor((seconds % 3600) / 60);
+
+  return `${hours}:${minutes}:${residue}`;
+}
+
+function cylinderVol(r, a) {
+  return Math.ceil(Math.PI * r * r * a);
+}
+
+function promedio(array) {
+  if (array.length === 0) return null
+
+  const sum = array.reduce( (previous, current) => { return previous + current })
+
+  return sum / array.length
+}
+
+
+
+module.exports = {
+  sum,
+  multiply,
+  divide,
+  secondsConverter,
+  promedio,
+  cylinderVol,
+};
